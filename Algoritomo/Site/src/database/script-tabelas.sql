0b804d054bfaafa8f139495c4bcf1746ba4820f5
Guilherme-Oliveira-Mendes <guilherme.mendes@sptech.school>
2025-05-26 19:49:18 -0300

    Implementacao Web-data-viz

---
+++ Algoritomo/Site/src/database/script-tabelas.sql
@@ -0,0 +1,14 @@
+create database Diario_da_galaxia;
+
+use Diario_da_galaxia;
+
+CREATE TABLE Usuario (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+	senha VARCHAR(10),
+	email VARCHAR(45)
+);
+
+
+
+select * from Usuario;
